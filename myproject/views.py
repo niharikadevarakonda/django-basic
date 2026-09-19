e10fd2498d12d1faa0284b9745989f047a70df46
Niharika_1907 <niharikadevarakonda81432@gmail.com>
2026-09-19 21:41:50 +0530

    Update views.py

--- myproject/views.py
+++ myproject/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
