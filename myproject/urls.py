9ee1ffb5b1abd262ac94fe4d09605e400a7fe198
Niharika_1907 <niharikadevarakonda81432@gmail.com>
2026-09-21 19:44:33 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('student/', views.student),
 ]
