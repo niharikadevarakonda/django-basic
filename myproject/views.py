63404d38acf8c48e42b6cb800e46bcac5e528b59
Niharika_1907 <niharikadevarakonda81432@gmail.com>
2026-09-21 19:42:20 +0530

    Add files via upload

--- myproject/views.py
+++ myproject/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def student(request):
+    data = {
+        'name': 'Niharika',
+        'age': 20,
+        'course': 'B.Tech',
+        'college': 'ISTS'
+    }
 
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'student.html', data)
