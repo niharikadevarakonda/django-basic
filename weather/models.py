7a2892b6b0cc4c0b67ab8fbbd0d4c05549d23902
Niharika_1907 <niharikadevarakonda81432@gmail.com>
2026-09-25 19:15:49 +0530

    Update models.py

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class WeatherData(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    description = models.CharField(max_length=200)
+    humidity = models.IntegerField()
+    wind_speed = models.FloatField()
+
+    def __str__(self):
+        return self.city
